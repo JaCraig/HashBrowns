4192174d8fead2f97ff793d871c45c92670e0627
James Craig <none@none>
2018-11-06 13:25:21 -0500

    Added tests and updated readme.

---
+++ HashBrowns.Tests/ExtensionMethodsTests.cs
@@ -0,0 +1,64 @@
+﻿using BigBook;
+using HashBrowns.Hashing.Enums;
+using HashBrowns.Symmetric.Enums;
+using HashBrowns.Tests.BaseClasses;
+using System.Text;
+using Xunit;
+
+namespace HashBrowns.Tests
+{
+    public class ExtensionMethodsTests : TestBaseClass
+    {
+        public static readonly TheoryData<HashingAlgorithms> HashTestData = new TheoryData<HashingAlgorithms>
+        {
+            {HashingAlgorithms.HMACMD5},
+            {HashingAlgorithms.HMACSHA1},
+            {HashingAlgorithms.HMACSHA256},
+            {HashingAlgorithms.HMACSHA384},
+            {HashingAlgorithms.HMACSHA512},
+            {HashingAlgorithms.MD5},
+            {HashingAlgorithms.SHA1},
+            {HashingAlgorithms.SHA256},
+            {HashingAlgorithms.SHA384},
+            {HashingAlgorithms.SHA512},
+        };
+
+        public static readonly TheoryData<SymmetricAlgorithms, int> SymmetricTestData = new TheoryData<SymmetricAlgorithms, int>
+        {
+            {SymmetricAlgorithms.AES,256},
+            {SymmetricAlgorithms.DES,64},
+            {SymmetricAlgorithms.RC2,128},
+            {SymmetricAlgorithms.Rijndael,256},
+            {SymmetricAlgorithms.TripleDES,192},
+        };
+
+        [Theory]
+        [MemberData(nameof(SymmetricTestData))]
+        public void Decrypt(SymmetricAlgorithms algorithms, int keySize)
+        {
+            var Key = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var IV = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6 };
+            Assert.Equal("Test String", "Test String".Encrypt((byte[])Key.Clone(),
+                    "Salt".ToByteArray(),
+                    HashingAlgorithms.SHA512,
+                    2,
+                    IV,
+                    keySize,
+                    algorithms)
+                    .Decrypt((byte[])Key.Clone(),
+                    "Salt".ToByteArray(),
+                    HashingAlgorithms.SHA512,
+                    2,
+                    IV,
+                    keySize,
+                    algorithms).ToString(Encoding.UTF8));
+        }
+
+        [Theory]
+        [MemberData(nameof(HashTestData))]
+        public void Hash(HashingAlgorithms algorithms)
+        {
+            Assert.NotNull("Test String".Hash(algorithms));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # HashBrowns
-A library to help simplify encryption within .Net.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/0e9q4k7van5qv3w3?svg=true)](https://ci.appveyor.com/project/JaCraig/hashbrowns)
+
+HashBrowns is a library to help simplify encryption within .Net.
+
+## Basic Usage
+
+The system relies on an IoC wrapper called [Canister](https://github.com/JaCraig/Canister). While Canister has a built in IoC container, it's purpose is to actually wrap your container of choice in a way that simplifies setup and usage for other libraries that don't want to be tied to a specific IoC container. HashBrowns uses it to detect and pull in various info. As such you must set up Canister in order to use HashBrowns:
+
+    Canister.Builder.CreateContainer(new List<ServiceDescriptor>())
+                    .RegisterHashBrowns()
+                    .Build();
+					
+This is required prior to using the CryptoManager or extension methods class for the first time. Once Canister is set up, you can use the CryptoManager class:
+
+    var Instance = Canister.Builder.Bootstrapper.Resolve<CryptoManager>().Encrypt(...);
+	
+However instead of having the IoC container create the class, you can also use the extension methods found in the HashBrowns namespace:
+
+    var EncryptedData = "My string that I want to encrypt".Encrypt(Key, Salt, HashingAlgorithm, NumberIterations, InitialVector, KeySize, EncryptionAlgorithm);
+
+Similarly you can decrypt:
+
+    var DecryptedData = EncryptedData.Encrypt(Key, Salt, HashingAlgorithm, NumberIterations, InitialVector, KeySize, EncryptionAlgorithm);
+
+Also hashing is available:
+
+    var MyHashedValue = "Example data".Hash(HashingAlgorithm);
+
+## Installation
+
+The library is available via Nuget with the package name "HashBrowns". To install it run the following command in the Package Manager Console:
+
+Install-Package HashBrowns
